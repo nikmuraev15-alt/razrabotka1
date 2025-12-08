18bfb0809f205d0b49c1bc0560b1c938e4bf1ef9
Никита <nik.muraev15@gmail.com>
2025-12-08 21:59:32 +0300

    connection.h

--- connection.h
+++ connection.h
@@ -1,3 +1,13 @@
+/**
+ * @file connection.h
+ * @author Мураев Н.Д.
+ * @version 1.0
+ * @date 2025
+ * @copyright ИБСТ ПГУ
+ * @brief Заголовочный файл для класса соединения
+ * @details Определяет класс Connection для управления сетевыми соединениями
+ */
+
 #pragma once
 #include "errno.h"
 #include "crypto.h"
@@ -9,9 +19,23 @@
 #include <sys/socket.h>
 #include <iostream>
 #include <fstream>
-#define BUFFER_SIZE 1024
+
+#define BUFFER_SIZE 1024 ///< Размер буфера для сетевых операций
+
 using namespace std;
+
+/**
+ * @class Connection
+ * @brief Класс для управления сетевыми соединениями сервера
+ * @details Обеспечивает установку соединения, аутентификацию клиентов и обработку данных
+ */
 class Connection{
-    public:
-int conn(const Params* p);
+public:
+    /**
+     * @brief Установка соединения и обработка клиентов
+     * @param p Указатель на параметры соединения
+     * @return Код завершения (0 - успех, 1 - ошибка)
+     * @throw std::system_error при ошибках сетевых операций
+     */
+    static int conn(const Params* p);
 };
