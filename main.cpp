bc3f9d2887a4fe28e932f25021380b25412fb608
Никита <nik.muraev15@gmail.com>
2025-12-08 22:04:33 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
+/**
+ * @file main.cpp
+ * @author Мураев Н.Д.
+ * @version 1.0
+ * @date 2025
+ * @copyright ИБСТ ПГУ
+ * @brief Главный модуль серверного приложения
+ * @details Основная точка входа в программу, обрабатывающая аргументы командной строки и запускающая сервер
+ */
+
 #include "connection.h"
 #include "interface.h"
 
+/**
+ * @brief Главная функция программы
+ * @param argc Количество аргументов командной строки
+ * @param argv Массив аргументов командной строки
+ * @return Код завершения программы (0 - успех, 1 - ошибка)
+ * @warning Для работы программы необходимо указать обязательные параметры
+ */
 int main(int argc, const char** argv)
 {
-    UserInterface interface;
-        if(!interface.Parser(argc, argv)){
-            cout << interface.getDescription() << endl;
-            return 1;
-        }
-    Params params = interface.getParams();
-    Connection::conn(&params);//&params
+    UserInterface userinterface;
+    if(!userinterface.Parser(argc, argv)){
+        cout << userinterface.getDescription() << endl;
+        return 1;
+    }
+    Params params = userinterface.getParams();
+    Connection::conn(&params);
     return 0;
 }
